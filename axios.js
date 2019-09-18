db765a429fa6717a16608ac508752561882259d6
Maximilian Schwarzmüller <company@academind.com>
2019-09-18 09:58:40 +0200

    finished axios

--- axios.js
+++ axios.js
@@ -1,9 +1,33 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {};
+const getData = () => {
+  axios.get('https://reqres.in/api/users').then(response => {
+    console.log(response);
+  });
+};
 
-const sendData = () => {};
+const sendData = () => {
+  axios
+    .post(
+      'https://reqres.in/api/register',
+      {
+        email: 'eve.holt@reqres.in'
+        // password: 'pistol'
+      },
+      {
+        // headers: {
+        //   'Content-Type': 'application/json'
+        // }
+      }
+    )
+    .then(response => {
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err, err.response);
+    });
+};
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
