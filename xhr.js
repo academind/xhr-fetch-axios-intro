146bef0f5781597fecb9c775c80ad9b5df706a4b
Maximilian Schwarzmüller <company@academind.com>
2019-09-18 09:25:16 +0200

    finished xhr

--- xhr.js
+++ xhr.js
@@ -1,9 +1,52 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {};
+const sendHttpRequest = (method, url, data) => {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
 
-const sendData = () => {};
+    xhr.responseType = 'json';
+
+    if (data) {
+      xhr.setRequestHeader('Content-Type', 'application/json');
+    }
+
+    xhr.onload = () => {
+      if (xhr.status >= 400) {
+        reject(xhr.response);
+      } else {
+        resolve(xhr.response);
+      }
+    };
+
+    xhr.onerror = () => {
+      reject('Something went wrong!');
+    };
+
+    xhr.send(JSON.stringify(data));
+  });
+  return promise;
+};
+
+const getData = () => {
+  sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData => {
+    console.log(responseData);
+  });
+};
+
+const sendData = () => {
+  sendHttpRequest('POST', 'https://reqres.in/api/register', {
+    email: 'eve.holt@reqres.in'
+    // password: 'pistol'
+  })
+    .then(responseData => {
+      console.log(responseData);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
