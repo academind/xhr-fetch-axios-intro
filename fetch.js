98b456de270a2e9e3519a145cc3d16fbc892eec0
Maximilian Schwarzmüller <company@academind.com>
2019-09-18 09:43:42 +0200

    added fetch

--- fetch.js
+++ fetch.js
@@ -1,9 +1,42 @@
 const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
-const getData = () => {};
+const sendHttpRequest = (method, url, data) => {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: data ? { 'Content-Type': 'application/json' } : {}
+  }).then(response => {
+    if (response.status >= 400) {
+      // !response.ok
+      return response.json().then(errResData => {
+        const error = new Error('Something went wrong!');
+        error.data = errResData;
+        throw error;
+      });
+    }
+    return response.json();
+  });
+};
 
-const sendData = () => {};
+const getData = () => {
+  sendHttpRequest('GET', 'https://reqres.in/api/users').then(responseData => {
+    console.log(responseData);
+  });
+};
+
+const sendData = () => {
+  sendHttpRequest('POST', 'https://reqres.in/api/register', {
+    email: 'eve.holt@reqres.in'
+    // password: 'pistol'
+  })
+    .then(responseData => {
+      console.log(responseData);
+    })
+    .catch(err => {
+      console.log(err, err.data);
+    });
+};
 
 getBtn.addEventListener('click', getData);
 postBtn.addEventListener('click', sendData);
