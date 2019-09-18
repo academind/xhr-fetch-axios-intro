cb2ab72e95e350424c7445ff084ca7e83f3964a9
Maximilian Schwarzmüller <company@academind.com>
2019-09-18 08:55:53 +0200

    starting setup

---
+++ xhr.js
@@ -0,0 +1,9 @@
+const getBtn = document.getElementById('get-btn');
+const postBtn = document.getElementById('post-btn');
+
+const getData = () => {};
+
+const sendData = () => {};
+
+getBtn.addEventListener('click', getData);
+postBtn.addEventListener('click', sendData);
